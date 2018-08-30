e4d7f7eaba5327fd02aa79e80fd30654e4da58f6
Paras <sivssdn@gmail.com>
2018-08-30 16:04:59 +0530

    sample output and label meanings added

--- entity_identifier.py
+++ entity_identifier.py
@@ -29,3 +29,26 @@ def entities(example, show=False):
 entities(example)
 
 
+# PERSON People, including fictional.
+# NORP Nationalities or religious or political groups.
+# FACILITY Buildings, airports, highways, bridges, etc.
+# ORG Companies, agencies, institutions, etc.
+# GPE Countries, cities, states. (That is, Geo-Political Entitites)
+# LOC Non-GPE locations, mountain ranges, bodies of water.
+# PRODUCT Objects, vehicles, foods, etc. (Not services.)
+# EVENT Named hurricanes, battles, wars, sports events, etc.
+# WORK_OF_ART Titles of books, songs, etc.
+# LANGUAGE Any named language.
+# LAW A legislation related entity(?)
+# DATE Absolute or relative dates or periods.
+# TIME Times smaller than a day.
+# PERCENT Percentage, including “%”.
+# MONEY Monetary values, including unit.
+# QUANTITY Measurements, as of weight or distance.
+# ORDINAL “first”, “second”, etc.
+# CARDINAL Numerals that do not fall under another type.
+
+'''
+Sample Output:
+{u'\n': [u'GPE'], u'776': [u'CARDINAL'], u'the Ancient Olympic Games': [u'EVENT'], u'\n Athletics': [u'ORG']}
+'''
